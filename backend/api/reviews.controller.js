e9ea7a914085accdb1b15f63314d8b5adc8aff61
Alexei196 <dndy778@gmail.com>
2023-02-12 13:51:54 -0800

    finishing backend

---
+++ backend/api/reviews.controller.js
@@ -0,0 +1,70 @@
+import ReviewsDAO from "../dao/reviewsDAO.js"
+import bson from "bson"
+const ObjectId = bson.ObjectId
+
+export default class ReviewsController {
+    static async apiPostReview(req, res, next) {
+        try{
+            const restaurantID = req.body.restaurantID
+            const review = req.body.text
+            const userInfo = {
+                name: req.body.name,
+                _id: req.body.user_id
+            }
+            const date = new Date()
+
+            const ReviewResponse = await ReviewsDAO.addReview(
+                ObjectId(restaurantID),
+                userInfo,
+                review,
+                date,
+            )
+            res.json({status: "success"})
+        } catch(e) {
+            res.status(500).json({error: e.message})
+        }
+    }
+
+    static async apiUpdateReview(req, res, next) {
+        try{
+            const reviewId = req.body.reviewId
+            const text = req.body.text
+            const date= new Date()
+
+            const reviewResponse = await ReviewsDAO.updateReview(
+                reviewId,
+                req.body.user_id,
+                text,
+                date,
+            )
+
+            var {error} = reviewResponse
+            if(error) {
+                req.status(400).json({error})
+            }
+
+            if(reviewResponse.modifiedCount === 0) {
+                throw new Error(
+                    "unable to update review",
+                )
+            }
+        } catch(e){
+            res.status(500).json({error: e.message})
+        }
+    }
+
+    static async apiDeleteReview(req, res, next){
+        try{
+            const reviewId = req.query.id
+            const userId = req.body.user_id
+
+            const reviewResponse = await ReviewsDAO.deleteReview(
+                reviewId,
+                userId
+            )
+            res.json({statis: "success"})
+        } catch(e) {
+            res.status(500).json({error: e.message})
+        }
+    }
+}
